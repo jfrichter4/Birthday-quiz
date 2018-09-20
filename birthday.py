01ed566dbd02920d4f97bd3dc69deef1dedbc105
jfrichter4 <42844330+jfrichter4@users.noreply.github.com>
2018-09-20 12:36:47 -0400

    Updated from Brython Server: 9/20/2018 12:36:49 PM

--- birthday.py
+++ birthday.py
@@ -1,3 +1,26 @@
+"""
+birthday.py
+Author: Joe
+Credit: None
+Assignment:
+Your program will ask the user the following questions, in this order:
+1. Their name.
+2. The name of the month they were born in (e.g. "September").
+3. The year they were born in (e.g. "1962").
+4. The day they were born on (e.g. "11").
+If the user's birthday fell on October 31, then respond with:
+  You were born on Halloween!
+If the user's birthday fell on today's date, then respond with:
+  Happy birthday!
+Otherwise respond with a statement like this:
+  Peter, you are a winter baby of the nineties.
+Example Session
+  Hello, what is your name? Eric
+  Hi Eric, what was the name of the month you were born in? September
+  And what year were you born in, Eric? 1972
+  And the day? 11
+  Eric, you are a fall baby of the stone age.
+"""
 from datetime import datetime
 from calendar import month_name
 todaymonth = datetime.today().month
