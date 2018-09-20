da7f48f2a5eeb5b27422a07262a061f7ea1086ab
jfrichter4 <42844330+jfrichter4@users.noreply.github.com>
2018-09-20 12:51:05 -0400

    Updated from Brython Server: 9/20/2018 12:51:09 PM

--- birthday.py
+++ birthday.py
@@ -33,108 +33,108 @@ Day = int(input("And the day? "))
 if month == todaymonth and Day == todaydate:
     print("Happy birthday!")
 elif month == "january" and year < 1980:
-    print("" + name + ", you are a Winter baby of the stone age.")
+    print("" + name + ", you are a winter baby of the stone age.")
 elif month == "october" and Day == 31:
     print("You were born on Halloween!")
 elif month == "february" and year < 1980:
-    print("" + name + ", you are a Winter baby of the stone age.")
+    print("" + name + ", you are a winter baby of the stone age.")
 elif month == "march" and year < 1980:
-    print("" + name + ", you are a Spring baby of the stone age.")
+    print("" + name + ", you are a spring baby of the stone age.")
 elif month == "april" and year < 1980:
-    print("" + name + ", you are a Spring baby of the stone age.")
+    print("" + name + ", you are a spring baby of the stone age.")
 elif month == "may" and year < 1980:
-    print("" + name + ", you are a Spring baby of the stone age.")
+    print("" + name + ", you are a spring baby of the stone age.")
 elif month == "june" and year < 1980:
-    print("" + name + ", you are a Summer baby of the stone age.")
+    print("" + name + ", you are a summer baby of the stone age.")
 elif month == "july" and year < 1980:
-    print("" + name + ", you are a Summer baby of the stone age.")
+    print("" + name + ", you are a summer baby of the stone age.")
 elif month == "august" and year < 1980:
-    print("" + name + ", you are a Summer baby of the stone age.")
+    print("" + name + ", you are a summer baby of the stone age.")
 elif month == "september" and year < 1980:
-    print("" + name + ", you are a Fall baby of the stone age.")
+    print("" + name + ", you are a fall baby of the stone age.")
 elif month == "october" and year < 1980:
-    print("" + name + ", you are a Fall baby of the stone age.")
+    print("" + name + ", you are a fall baby of the stone age.")
 elif month == "november" and year < 1980:
-    print("" + name + ", you are a Fall baby of the stone age.")
+    print("" + name + ", you are a fall baby of the stone age.")
 elif month == "december" and year < 1980:
-    print("" + name + ", you are a Winter baby of the stone age.")
+    print("" + name + ", you are a winter baby of the stone age.")
 elif month == "january" and year > 1980 and year <= 1989:
-    print("" + name + ", you are a Winter baby of the eighties.")
+    print("" + name + ", you are a winter baby of the eighties.")
 elif month == "october" and Day == 31:
     print("You were born on Halloween!")
 elif month == "february" and year > 1980 and year <= 1989:
-    print("" + name + ", you are a Winter baby of the eighties.")
+    print("" + name + ", you are a winter baby of the eighties.")
 elif month == "march" and year > 1980 and year <= 1989:
-    print("" + name + ", you are a Spring baby of the eighties.")
+    print("" + name + ", you are a spring baby of the eighties.")
 elif month == "april" and year > 1980 and year <= 1989:
-    print("" + name + ", you are a Spring baby of the eighties.")
+    print("" + name + ", you are a spring baby of the eighties.")
 elif month == "may" and year > 1980 and year <= 1989:
-    print("" + name + ", you are a Spring baby of the eighties.")
+    print("" + name + ", you are a spring baby of the eighties.")
 elif month == "june" and year > 1980 and year <= 1989:
-    print("" + name + ", you are a Summer baby of the eighties.")
+    print("" + name + ", you are a summer baby of the eighties.")
 elif month == "july" and year > 1980 and year <= 1989:
-    print("" + name + ", you are a Summer baby of the eighties.")
+    print("" + name + ", you are a summer baby of the eighties.")
 elif month == "august" and year > 1980 and year <= 1989:
-    print("" + name + ", you are a Summer baby of the eighties.")
+    print("" + name + ", you are a summer baby of the eighties.")
 elif month == "september" and year > 1980 and year <= 1989:
-    print("" + name + ", you are a Fall baby of the eighties.")
+    print("" + name + ", you are a fall baby of the eighties.")
 elif month == "october" and year > 1980 and year <= 1989:
-    print("" + name + ", you are a Fall baby of the eighties.")
+    print("" + name + ", you are a fall baby of the eighties.")
 elif month == "november" and year > 1980 and year <= 1989:
-    print("" + name + ", you are a Fall baby of the eighties.")
+    print("" + name + ", you are a fall baby of the eighties.")
 elif month == "december" and year > 1980 and year <= 1989:
-    print("" + name + ", you are a Winter baby of the eighties.")
+    print("" + name + ", you are a winter baby of the eighties.")
 elif month == "january" and year > 1990 and year <= 1999:
-    print("" + name + ", you are a Winter baby of the nineties.")
+    print("" + name + ", you are a winter baby of the nineties.")
 elif month == "october" and Day == 31:
     print("You were born on Halloween!")
 elif month == "february" and year > 1990 and year <= 1999:
-    print("" + name + ", you are a Winter baby of the nineties.")
+    print("" + name + ", you are a winter baby of the nineties.")
 elif month == "march" and year > 1990 and year <= 1999:
-    print("" + name + ", you are a Spring baby of the nineties.")
+    print("" + name + ", you are a spring baby of the nineties.")
 elif month == "april" and year > 1990 and year <= 1999:
-    print("" + name + ", you are a Spring baby of the nineties.")
+    print("" + name + ", you are a spring baby of the nineties.")
 elif month == "may" and year > 1990 and year <= 1999:
-    print("" + name + ", you are a Spring baby of the nineties.")
+    print("" + name + ", you are a spring baby of the nineties.")
 elif month == "june" and year > 1990 and year <= 1999:
-    print("" + name + ", you are a Summer baby of the nineties.")
+    print("" + name + ", you are a summer baby of the nineties.")
 elif month == "july" and year > 1990 and year <= 1999:
-    print("" + name + ", you are a Summer baby of the nineties.")
+    print("" + name + ", you are a summer baby of the nineties.")
 elif month == "august" and year > 1990 and year <= 1999:
-    print("" + name + ", you are a Summer baby of the nineties.")
+    print("" + name + ", you are a summer baby of the nineties.")
 elif month == "september" and year > 1990 and year <= 1999:
-    print("" + name + ", you are a Fall baby of the nineties.")
+    print("" + name + ", you are a fall baby of the nineties.")
 elif month == "october" and year > 1990 and year <= 1999:
-    print("" + name + ", you are a Fall baby of the nineties.")
+    print("" + name + ", you are a fall baby of the nineties.")
 elif month == "november" and year > 1990 and year <= 1999:
-    print("" + name + ", you are a Fall baby of the nineties.")
+    print("" + name + ", you are a fall baby of the nineties.")
 elif month == "december" and year > 1990 and year <= 1999:
-    print("" + name + ", you are a Winter baby of the nineties.")
+    print("" + name + ", you are a winter baby of the nineties.")
 elif month == "january" and year >= 2000:
-    print("" + name + ", you are a Winter baby of the 2000s.")
+    print("" + name + ", you are a winter baby of the 2000s.")
 elif month == "october" and Day == 31:
     print("You were born on Halloween!")
 elif month == "february" and year >= 2000:
-    print("" + name + ", you are a Winter baby of the 2000s.")
+    print("" + name + ", you are a winter baby of the 2000s.")
 elif month == "march" and year >= 2000:
-    print("" + name + ", you are a Spring baby of the 2000s.")
+    print("" + name + ", you are a spring baby of the 2000s.")
 elif month == "april" and year >= 2000:
-    print("" + name + ", you are a Spring baby of the 2000s.")
+    print("" + name + ", you are a spring baby of the 2000s.")
 elif month == "may" and year >= 2000:
-    print("" + name + ", you are a Spring baby of the 2000s.")
+    print("" + name + ", you are a spring baby of the 2000s.")
 elif month == "june" and year >= 2000:
-    print("" + name + ", you are a Summer baby of the 2000s.")
+    print("" + name + ", you are a summer baby of the 2000s.")
 elif month == "july" and year >= 2000:
-    print("" + name + ", you are a Summer baby of the 2000s.")
+    print("" + name + ", you are a summer baby of the 2000s.")
 elif month == "august" and year >= 2000:
-    print("" + name + ", you are a Summer baby of the 2000s.")
+    print("" + name + ", you are a summer baby of the 2000s.")
 elif month == "september" and year >= 2000:
-    print("" + name + ", you are a Fall baby of the 2000s.")
+    print("" + name + ", you are a fall baby of the 2000s.")
 elif month == "october" and year >= 2000:
-    print("" + name + ", you are a Fall baby of the 2000s.")
+    print("" + name + ", you are a fall baby of the 2000s.")
 elif month == "november" and year >= 2000:
-    print("" + name + ", you are a Fall baby of the 2000s.")
+    print("" + name + ", you are a fall baby of the 2000s.")
 elif month == "december" and year >= 2000:
-    print("" + name + ", you are a Winter baby of the 2000s.")
+    print("" + name + ", you are a winter baby of the 2000s.")
 elif month == "october" and Day == 31:
     print("You were born on Halloween!")
